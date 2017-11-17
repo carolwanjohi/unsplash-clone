a2a9371278d39fa8ac0bfa37314f5db333816a36
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-17 11:49:02 +0300

    Create index page Create base.html as the parent template for all templates in app-posts folder Create index.html to be rendered by the index view function Set index view function as the home route

--- unsplash/clone/urls.py
+++ unsplash/clone/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url( r'^$', views.index, name="index"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
+# if settings.DEBUG:
+#     urlpatterns += static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
     
