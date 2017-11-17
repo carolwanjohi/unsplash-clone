68de4eb9e5af1ffa8a80bfd3f5e0fc84cd04b007
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-17 12:25:35 +0300

    Create User model Update database to PostgreSQL Create User class for the application users Create test case for the instance of the User class Create test case for the save user method in the User class Create test case for the delete user method in the User class Create test case for the get users class method in the User class Create migration to database with users table
    Creaete tags model
    Create tags class for the categories of photos
    Create test case for the instance of the tags class
    Create test case for the save user method in the tags class
    Create test case for the delete user method in the tags class
    Create test case for the get users class method in the tags class
    Create migration to database with tags table

--- unsplash/clone/models.py
+++ unsplash/clone/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    '''
+    Class that defines a User for the application
+    '''
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ['first_name']
+
+    def save_user(self):
+        '''
+        Method to save a new User to the database
+        '''
+        self.save()
+
+    def delete_user(self):
+        '''
+        Method to delete a User from the database
+        '''
+        self.delete()
+
+    @classmethod
+    def get_users(cls):
+        '''
+        Method that gets all users from the database
+
+        Returns:
+            users : list of user objects from the database
+        '''
+        users = User.objects.all()
+        return users
+
+class tags(models.Model):
+    '''
+    Class that defines categories of photographs and tags on image posts
+    '''
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_tag(self):
+        '''
+        Method to save a new tag to the database
+        '''
+        self.save()
+
+    def delete_tag(self):
+        '''
+        Method to delete a tag from the database
+        '''
+        self.delete()
+
+    @classmethod
+    def get_tags(cls):
+        '''
+        Method that gets all tags from the database
+
+        Returns:
+            gotten_tags : list of tag objects from the database
+        '''
+        gotten_tags = tags.objects.all()
+        return gotten_tags
+
+
--- unsplash/clone/tests.py
+++ unsplash/clone/tests.py
@@ -1,3 +1,96 @@
 from django.test import TestCase
+from .models import User, tags
 
 # Create your tests here.
+class UserTestClass(TestCase):
+    '''
+    Test case for User class
+    '''
+
+    # Set Up method
+    def setUp(self):
+        '''
+        Method that sets up a User instance before each test
+        '''
+        # Create a user instance
+        self.new_user = User(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_user in an instance of User class
+        '''
+        self.assertTrue( isinstance(self.new_user, User) )
+
+    def test_save_user(self):
+        '''
+        Test case to check is a user is saved in the database
+        '''
+        self.new_user.save_user()
+        users = User.objects.all()
+        self.assertTrue( len(users) > 0 )
+
+    def test_delete_user(self):
+        '''
+        Test case to check if a user is deleted from the database
+        '''
+        self.new_user.save_user()
+        users = User.objects.all()
+        self.new_user.delete_user()
+        self.assertTrue( len(users) == 0)
+
+    def test_get_users(self):
+        '''
+        Test case to check if all users are gotten from the database
+        '''
+        self.new_user.save_user()
+        gotten_users = User.get_users()
+        users = User.objects.all()
+        self.assertTrue( len(gotten_users) == len(users))
+
+class tagsTestClass(TestCase):
+    '''
+    Test case for tags class
+    '''
+
+    # Set Up method
+    def setUp(self):
+        '''
+        Method that sets up a tags instance before each test
+        '''
+        self.new_tag = tags(name='Python')
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_tag in an instance of tags class
+        '''
+        self.assertTrue( isinstance(self.new_tag, tags) )
+
+    def test_save_editor(self):
+        '''
+        Test case to check is a tag is saved in the database
+        '''
+        self.new_tag.save_tag()
+        gotten_tags = tags.objects.all()
+        self.assertTrue( len(gotten_tags) > 0 )
+
+    def test_delete_tag(self):
+        '''
+        Test case to check if a tag is deleted from the database
+        '''
+        self.new_tag.save_tag()
+        gotten_tags = tags.objects.all()
+        self.new_tag.delete_tag()
+        self.assertTrue( len(gotten_tags) == 0 )
+
+    def test_get_tags(self):
+        '''
+        Test case to check if all tags are gotten from the database
+        '''
+        self.new_tag.save_tag()
+        gotten_tags = tags.get_tags()
+        existing_tags = tags.objects.all()
+        self.assertTrue( len(gotten_tags) == len(existing_tags))
+
+
+
+
