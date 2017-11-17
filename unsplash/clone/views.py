45f4c278f84388303483a75eedec2d746768087f
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-17 14:49:59 +0300

    Display tags Update index view function to get tags from the database Update index.html to display a specific number of tags Update index.html and navbar.html to have there own stylesheets Change the background color and border color of the navbar Change color of the site name

--- unsplash/clone/views.py
+++ unsplash/clone/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
+from .models import tags, ImagePost
 
 # Create your views here.
 def index(request):
     '''
     View function that returns a list of tags and images on the home page
     '''
-    message = "Home page"
+    gotten_tags = tags.get_tags()
     title = 'hsalpsnU Home'
-    return render(request, 'all-posts/index.html', {"message":message, "title":title})
+    return render(request, 'all-posts/index.html', {"title":title, "tags":gotten_tags})
 
