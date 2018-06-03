c2abd8e7ac2ade4949fec9af4a5167c79da5ca1c
Carol Wanjohi <wanjohi.carol54@gmail.com>
2018-06-03 11:36:23 +0300

    Update Database Configuration * Update DATABASES to get USER and PASSWORD from environments file

--- unsplash/settings.py
+++ unsplash/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'unsplash',
-        'USER': ,
-        'PASSWORD': ,
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
     }
 }
 import dj_database_url
